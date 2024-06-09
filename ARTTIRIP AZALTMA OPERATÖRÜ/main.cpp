be6ab312b82799981c470dce1673a7559a8ad57d
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-06-10 01:26:47 +0300

    Add files via upload

---
+++ ARTTIRIP AZALTMA OPERATÖRÜ/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int a,b,c,d;
+	a=10 , b=20 , c=30 , d=40;
+	
+	//cout<< a << endl;
+	//a++, b++, c++, d++;
+	
+	a=a+1;
+	b=b+1;
+	c=c+1;
+	d=d+1;
+	cout<< a << endl;
+	cout<< b << endl;
+	cout<< c << endl;
+	cout<< d << endl;
+	
+	
+	
+	
+	return 0;
+}
