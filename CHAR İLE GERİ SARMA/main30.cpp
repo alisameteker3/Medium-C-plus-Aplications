5f445977f555589bf4f322b7b702549232797401
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-06-11 15:28:39 +0300

    Add files via upload

---
+++ CHAR İLE GERİ SARMA/main30.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+
+ char a[7]={'A', 'K', 'S', 'A', 'R' , 'A', 'Y'};
+ 	 
+ 	 for(int i = 6 ; i >=0 ; i--){
+ 	 	
+ 	 	cout<< a[i];
+	  }
+
+
+
+
+
+
+	return 0;
+}
