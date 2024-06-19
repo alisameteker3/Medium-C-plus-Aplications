b3dcca820f4fd72da4541e698a547f3b34262e21
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-06-19 15:08:59 +0300

    Add files via upload

---
+++ POLİNDROM SAYI/polindromSayi/polindromSayi.cpp
@@ -0,0 +1,32 @@
+﻿// polindromSayi.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int num, digit, sum = 0;
+	int temp;
+	cout << "Bir sayi girin : ";
+	cin >> num;
+	temp = num;
+
+	while ( num!=0)
+	{
+		digit = num % 10;
+		sum = sum * 10 + digit;
+		num /= 10;
+
+	}
+	if (temp == sum)
+	{
+		cout << "sayi polindrom bir sayidir " << endl;
+	}
+	else
+		cout << "polindirom bir sayi degil "<<endl;
+
+
+	return 0;
+
+}
