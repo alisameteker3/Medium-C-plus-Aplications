d41b05e94ab20952c558231effdf92721f4b84f6
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-06-19 15:04:20 +0300

    Add files via upload

---
+++ STOPWATCH/klonometreYapımı/klonometreYapımı.cpp
@@ -0,0 +1,35 @@
+﻿// klonometreYapımı.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+#include <Windows.h> //sleep kütüphanesi sleep()
+
+
+
+using namespace std;
+
+int main()
+{
+	int saniye = 0;
+	int dakika = 0;
+ 
+	while (true)
+	{
+		Sleep(1000); 
+		system("cls");
+
+		cout << dakika << " dakika :  " << saniye << " saniye "<<endl;
+		saniye++;	
+		
+		if (saniye == 60) {
+			dakika++;
+			saniye = 0;
+ 
+		}
+	}
+
+	system("pause");
+	return 0;
+}
+
+ 
