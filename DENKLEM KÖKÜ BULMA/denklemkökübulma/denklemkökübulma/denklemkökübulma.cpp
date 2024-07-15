87d8bdf487b52520a72537c6dfaa8762189a1f08
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-07-15 21:51:39 +0300

    Add files via upload

---
+++ DENKLEM KÖKÜ BULMA/denklemkökübulma/denklemkökübulma/denklemkökübulma.cpp
@@ -0,0 +1,42 @@
+﻿ 
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+	int a, b, c;
+	float delta;
+	cout << "x2 kat degerini girin : ";
+	cin >> a;
+	cout << "x kat degerini girin : ";
+	cin >> b;
+	cout << "sabit sayiyi girin : ";
+	cin >> c;
+
+	delta = (b * b) - 4 * a * c;
+	
+	if (delta < 0) {
+		cout << "Reel kok yoktur. ";
+	}
+	else if (delta == 0) {
+
+		cout << "cakisan iki kok vardir. ";
+
+	}
+	else if (delta > 0) {
+
+		cout << "birbirinden farkli iki kok vardir. " << endl;
+	}
+	float kok1 = (-b + sqrt(delta)) / (2 * a);
+	float kok2 = (-b - sqrt(delta)) / (2 * a);
+
+	cout << "x1 : " << kok1 << endl;
+	cout << "x2 : " << kok2 << endl;
+
+
+	return 0;
+
+
+}
+ 
