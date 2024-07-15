d7af5f813e2687e0272c86524c49a16d023aa80a
Ali Samet Eker <146390419+alisameteker3@users.noreply.github.com>
2024-07-15 21:53:14 +0300

    Add files via upload

---
+++ HACİM HESAPLAYAN FONKSİYON/hacim hesabi yapan fonksiyon/hacimhesabiFonsy/hacimhesabiFonsy.cpp
@@ -0,0 +1,54 @@
+﻿// hacimhesabiFonsy.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
+//
+
+#include <iostream>
+using namespace std;
+float alan(int, float, float);
+
+int main()
+{
+	int sekil;
+	float h, r;
+	cout << "1. Silindir hacim hesapla : " << endl;
+	cout << "2. Koni hacim hesapla : " << endl;
+	cout << "3. Kare piramit hacim hesapla : " << endl;
+	cout << "Seciminiz : " << endl;
+	cin >> sekil;
+	if (sekil !=3)
+	{
+		cout << "Yukseklik Giriniz : " << endl;
+		cin >> h;
+		cout << "r degeri giriniz : " << endl;
+		cin >> r;
+		cout << "Alan : " << alan(sekil, h, r) << endl;
+	}
+	else {
+		cout << "Yukseklik Giriniz : " << endl;
+		cin >> h;
+		cout << "a degeri giriniz : " << endl;
+		cin >> r;
+		cout << "Alan : " << alan(sekil, h, r) << endl;
+	}
+ 
+	system("pause");
+	return 0;
+}
+float alan(int sekil, float h, float r) {
+		
+	switch (sekil)
+	{
+	case1:
+		return (3.14f * (r * r) * h);
+		break;
+	case 2:
+		return (1.f/3.f) * (3.14f * (r * r) * h);
+		break;
+	case 3:
+		return ((1.f / 3.f) * (r * r) * h);
+		break;
+	default:
+		break;
+	}
+	//return 0;
+}
+ 
